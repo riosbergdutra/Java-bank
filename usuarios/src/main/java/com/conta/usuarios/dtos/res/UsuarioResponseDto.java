bec87f5f5c784b2dbbf2d9c3f250d4ae9e3ed6b3
Riosberg Dutra <riosbergduttra@gmail.com>
2024-04-28 16:03:19 -0300

    convertor de objeto para string virse e versa

--- usuarios/src/main/java/com/conta/usuarios/dtos/res/UsuarioResponseDto.java
+++ usuarios/src/main/java/com/conta/usuarios/dtos/res/UsuarioResponseDto.java
@@ -4,6 +4,8 @@ package com.conta.usuarios.dtos.res;
 import java.sql.Date;
 
 import com.conta.usuarios.enums.TipoConta;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public record UsuarioResponseDto(
     String nome,
@@ -14,5 +16,26 @@ public record UsuarioResponseDto(
     TipoConta tipoConta,
     Date dataConta
 ) {
+
+   public static UsuarioResponseDto fromJson(String json) {
+      if (json == null || json.isEmpty()) {
+          throw new IllegalArgumentException("A string JSON fornecida está vazia ou nula");
+      }
+      try {
+          ObjectMapper objectMapper = new ObjectMapper();
+          return objectMapper.readValue(json, UsuarioResponseDto.class);
+      } catch (JsonProcessingException e) {
+          throw new IllegalArgumentException("Erro ao converter JSON para UsuarioDto", e);
+      }
+  }
+
+   public String toJson() {  
+     try {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(this);
+    } catch (JsonProcessingException e) {
+        throw new RuntimeException("Erro ao converter UsuarioDto para JSON", e);
+    }
+  } 
 
 }
