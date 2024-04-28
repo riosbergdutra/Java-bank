269c5e85f420d93c9797445eee121d985b9593f0
Riosberg Dutra <riosbergduttra@gmail.com>
2024-04-28 16:04:05 -0300

    trocando long para uuid

--- usuarios/src/main/java/com/conta/usuarios/repository/UsuarioRepository.java
+++ usuarios/src/main/java/com/conta/usuarios/repository/UsuarioRepository.java
@@ -1,11 +1,15 @@
 package com.conta.usuarios.repository;
 
+
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.conta.usuarios.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario,UUID> {
     Usuario findByCpf(String cpf);
 } 
