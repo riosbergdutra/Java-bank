b41e5f8778d26f68478aa166a7d3021c63a4399d
Riosberg Dutra <riosbergduttra@gmail.com>
2024-04-28 15:59:50 -0300

    adicionando uuid

--- usuarios/src/main/resources/data.sql
+++ usuarios/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 CREATE TABLE usuarios (
-    id_usuario SERIAL PRIMARY KEY,
+    id_usuario UUID PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
