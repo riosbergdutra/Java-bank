e47450b17783ac02cbd8c7e395cc97b714b3315f
Riosberg Dutra <riosbergduttra@gmail.com>
2024-04-28 16:01:00 -0300

    implementando uuid nos controllers

--- usuarios/src/main/java/com/conta/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/com/conta/usuarios/controller/UsuarioController.java
@@ -3,12 +3,15 @@ package com.conta.usuarios.controller;
 import com.conta.usuarios.dtos.req.UsuarioRequestDto;
 import com.conta.usuarios.dtos.res.UsuarioResponseDto;
 import com.conta.usuarios.services.UsuarioService;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -17,6 +20,8 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+ 
+
     @PostMapping("/criar")
     public ResponseEntity<UsuarioResponseDto> criarUsuario(@RequestBody UsuarioRequestDto usuarioDto) {
         UsuarioResponseDto responseDto = usuarioService.criarUsuarioService(usuarioDto);
@@ -24,7 +29,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioResponseDto> buscarUsuarioPorId(@PathVariable Long id) {
+    public ResponseEntity<UsuarioResponseDto> buscarUsuarioPorId(@PathVariable UUID id) {
         UsuarioResponseDto responseDto = usuarioService.buscarUsuarioPorIdService(id);
         if (responseDto != null) {
             return new ResponseEntity<>(responseDto, HttpStatus.OK);
@@ -40,7 +45,7 @@ public class UsuarioController {
     }
 
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<UsuarioResponseDto> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDto usuarioDto) {
+    public ResponseEntity<UsuarioResponseDto> atualizarUsuario(@PathVariable UUID id, @RequestBody UsuarioRequestDto usuarioDto) {
         UsuarioResponseDto responseDto = usuarioService.atualizarUsuarioService(id, usuarioDto);
         if (responseDto != null) {
             return new ResponseEntity<>(responseDto, HttpStatus.OK);
@@ -50,7 +55,7 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+    public ResponseEntity<Void> deletarUsuario(@PathVariable UUID id) {
         usuarioService.deletarUsuarioService(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
