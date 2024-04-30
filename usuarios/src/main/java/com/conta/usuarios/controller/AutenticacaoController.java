7d1c3281db4f4b095db4fc458fd2a840e8a1f914
Riosberg Dutra <riosbergduttra@gmail.com>
2024-04-30 16:11:37 -0300

    adicionando o dto de token

--- usuarios/src/main/java/com/conta/usuarios/controller/AutenticacaoController.java
+++ usuarios/src/main/java/com/conta/usuarios/controller/AutenticacaoController.java
@@ -1,14 +1,11 @@
 package com.conta.usuarios.controller;
-
 import com.conta.usuarios.dtos.AuthDto;
+import com.conta.usuarios.dtos.res.LoginResponseDTO;
 import com.conta.usuarios.services.AutenticacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,10 +15,10 @@ public class AutenticacaoController {
     private AutenticacaoService autenticacaoService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AuthDto authDto) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthDto authDto) {
         String token = autenticacaoService.obterToken(authDto);
         if (token != null) {
-            return new ResponseEntity<>(token, HttpStatus.OK);
+            return new ResponseEntity<>(new LoginResponseDTO(token), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
