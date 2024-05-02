2cdcc706aff774170744b8cc4f6e05f0bf3e0256
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-02 16:24:11 -0300

    inicio contabancaria

---
+++ contabancaria/src/main/java/com/contabancaria/contabancaria/ContabancariaApplication.java
@@ -0,0 +1,13 @@
+package com.contabancaria.contabancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabancariaApplication.class, args);
+	}
+
+}
---
+++ contabancaria/src/test/java/com/contabancaria/contabancaria/ContabancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contabancaria.contabancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
