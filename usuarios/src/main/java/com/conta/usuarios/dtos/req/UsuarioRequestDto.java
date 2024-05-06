e6058e06e35666ca8b09bb5228bc1f0e69ca7c24
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-06 16:35:40 -0300

    tirando codigo inutil

--- usuarios/src/main/java/com/conta/usuarios/dtos/req/UsuarioRequestDto.java
+++ usuarios/src/main/java/com/conta/usuarios/dtos/req/UsuarioRequestDto.java
@@ -8,8 +8,7 @@ import java.util.UUID;
 
 import com.conta.usuarios.enums.RoleEnum;
 import com.conta.usuarios.enums.TipoConta;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public record UsuarioRequestDto(
    UUID id_usuario,
@@ -20,26 +19,5 @@ public record UsuarioRequestDto(
    RoleEnum role,
    TipoConta tipoConta,
    LocalDate dataConta) {
-
-    public static UsuarioRequestDto fromJson(String json) {
-      if (json == null || json.isEmpty()) {
-          throw new IllegalArgumentException("A string JSON fornecida está vazia ou nula");
-      }
-      try {
-          ObjectMapper objectMapper = new ObjectMapper();
-          return objectMapper.readValue(json, UsuarioRequestDto.class);
-      } catch (JsonProcessingException e) {
-          throw new IllegalArgumentException("Erro ao converter JSON para UsuarioDto", e);
-      }
-  }
-
-   public String toJson() {  
-     try {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(this);
-    } catch (JsonProcessingException e) {
-        throw new RuntimeException("Erro ao converter UsuarioDto para JSON", e);
-    }
-  } 
     
 }
--- usuarios/src/main/java/com/conta/usuarios/dtos/res/UsuarioResponseDto.java
+++ usuarios/src/main/java/com/conta/usuarios/dtos/res/UsuarioResponseDto.java
@@ -4,8 +4,6 @@ package com.conta.usuarios.dtos.res;
 import java.time.LocalDate;
 
 import com.conta.usuarios.enums.TipoConta;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public record UsuarioResponseDto(
     String nome,
@@ -14,28 +12,5 @@ public record UsuarioResponseDto(
     TipoConta tipoConta,
     LocalDate dataConta
 ) {
-
-   
-
-public static UsuarioResponseDto fromJson(String json) {
-      if (json == null || json.isEmpty()) {
-          throw new IllegalArgumentException("A string JSON fornecida está vazia ou nula");
-      }
-      try {
-          ObjectMapper objectMapper = new ObjectMapper();
-          return objectMapper.readValue(json, UsuarioResponseDto.class);
-      } catch (JsonProcessingException e) {
-          throw new IllegalArgumentException("Erro ao converter JSON para UsuarioDto", e);
-      }
-  }
-
-   public String toJson() {  
-     try {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(this);
-    } catch (JsonProcessingException e) {
-        throw new RuntimeException("Erro ao converter UsuarioDto para JSON", e);
-    }
-  } 
 
 }
