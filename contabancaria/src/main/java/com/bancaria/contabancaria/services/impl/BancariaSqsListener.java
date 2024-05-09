b3cf73cc49a1684b8f5b11d1b17e5a29a2370f42
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-09 15:55:16 -0300

    adicionando um ouvidor para sqs

---
+++ contabancaria/src/main/java/com/bancaria/contabancaria/services/impl/BancariaSqsListener.java
@@ -0,0 +1,29 @@
+package com.bancaria.contabancaria.services.impl;
+
+import org.springframework.stereotype.Component;
+import io.awspring.cloud.sqs.annotation.SqsListener;
+
+import com.bancaria.contabancaria.services.BancariaService;
+
+@Component
+public class BancariaSqsListener {
+    private final BancariaService bancariaService;
+
+    public BancariaSqsListener(BancariaService bancariaService) {
+        this.bancariaService = bancariaService;
+    }
+
+    @SqsListener("usuarios")
+    public void receberMensagemSQS(String mensagemSQS) {
+        try {
+            // Aqui você pode processar a mensagem SQS como uma String simples
+            System.out.println("Mensagem SQS recebida: " + mensagemSQS);
+
+            // Você pode chamar o serviço de conta bancária diretamente ou converter a String para o formato desejado, se necessário
+            bancariaService.processarMensagemSQS(mensagemSQS);
+        } catch (Exception e) {
+            // Tratamento de exceção adequado, se necessário
+            e.printStackTrace();
+        }
+    }
+}
