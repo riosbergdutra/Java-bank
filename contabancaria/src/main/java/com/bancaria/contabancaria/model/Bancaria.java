e3f963b688ce144b94305c71a0b8ed0927d4f157
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-09 15:53:12 -0300

    colocando construtores

--- contabancaria/src/main/java/com/bancaria/contabancaria/model/Bancaria.java
+++ contabancaria/src/main/java/com/bancaria/contabancaria/model/Bancaria.java
@@ -14,13 +14,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "bancaria")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bancaria {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
