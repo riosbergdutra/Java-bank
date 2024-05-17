d5c3347277d03440c724d3bdea487fa7ab17d808
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-17 16:32:56 -0300

    adicionando comentarios e colocando novos metodos

--- contabancaria/src/main/java/com/bancaria/contabancaria/services/BancariaService.java
+++ contabancaria/src/main/java/com/bancaria/contabancaria/services/BancariaService.java
@@ -2,11 +2,29 @@ package com.bancaria.contabancaria.services;
 
 import java.util.UUID;
 
-import com.bancaria.contabancaria.dtos.req.BancariaRequestDto;
-import com.bancaria.contabancaria.dtos.res.BancariaResponseDto;
+import com.bancaria.contabancaria.dtos.bancaria.request.BancariaRequestDto;
+import com.bancaria.contabancaria.dtos.bancaria.response.BancariaResponseDto;
+import com.bancaria.contabancaria.dtos.chave.ChaveDto;
+import com.bancaria.contabancaria.dtos.deposito.request.DepositoRequestDto;
+import com.bancaria.contabancaria.dtos.deposito.response.DepositoResponseDto;
+import com.bancaria.contabancaria.dtos.transferencia.request.TransferenciaRequestDto;
+import com.bancaria.contabancaria.dtos.transferencia.response.TransferenciaResponseDto;
 
 public interface BancariaService {
+
+    //realiza transferencia pelas chaves
+    TransferenciaResponseDto realizarTransferencia(TransferenciaRequestDto transferenciaDto);
+
+    // recebe mensagem sqs e cria a bancaria para o usuario id
     BancariaResponseDto criarBancariaService(BancariaRequestDto bancariaDto);
+
+    //adiciona uma chave para permitir fazer transferencia e etc...
+    ChaveDto adicionarChave(UUID id, ChaveDto chaveDto);
+    
+    DepositoResponseDto depositar(DepositoRequestDto depositoRequestDto);
+
+    // recebe mensagem sqs e deleta a bancaria para o usuario id
     void deletarBancariaService(UUID idUsuario);
+    // recebe mensagem sqs e decide oque fará em seguida(criar conta ou deletar a conta)
     void processarMensagemSQS(String mensagemSQS);
 }
