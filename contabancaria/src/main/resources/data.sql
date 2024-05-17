08b9564747eb70a4b6bb65137ee0f254f2876c65
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-17 16:30:44 -0300

    adicionando chave no sql

--- contabancaria/src/main/resources/data.sql
+++ contabancaria/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS bancaria (
     id_bancaria UUID PRIMARY KEY,
     id_usuario UUID REFERENCES usuarios(id_usuario)  NOT NULL,
+    chave VARCHAR(255) UNIQUE,
     tipo_conta VARCHAR(255) NOT NULL,
     saldo NUMERIC NOT NULL,
     data_conta DATE NOT NULL,
