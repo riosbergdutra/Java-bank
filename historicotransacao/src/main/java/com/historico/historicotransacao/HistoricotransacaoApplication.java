6bcea2a85d174abcafa10767e8dc75c518ac4d3a
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-18 14:05:32 -0300

    arquivos bases para transacoes

---
+++ historicotransacao/src/main/java/com/historico/historicotransacao/HistoricotransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.historico.historicotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricotransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricotransacaoApplication.class, args);
+	}
+
+}
---
+++ historicotransacao/src/test/java/com/historico/historicotransacao/HistoricotransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historico.historicotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricotransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
