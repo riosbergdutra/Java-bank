f8317683f4c5a2b87a1469ccb1808f4988c0e6bf
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-19 17:24:32 -0300

    mudando dado

--- historicotransacao/src/main/resources/data.sql
+++ historicotransacao/src/main/resources/data.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS historico (
     id_bancariadestino UUID NOT NULL,
     id_usuariodestino UUID NOT NULL,
     chavedestino VARCHAR(255) NOT NULL
-    valor_transacao NUMERIC NOT NULL,
+    valor NUMERIC NOT NULL,
     tipo_transacao VARCHAR(255) NOT NULL,
     data_transacao DATE NOT NULL
 );
