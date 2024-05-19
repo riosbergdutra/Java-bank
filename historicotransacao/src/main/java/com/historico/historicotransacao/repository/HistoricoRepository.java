4b7f807be75a16de22108f1373f7842783808fbc
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-19 17:23:19 -0300

    colocando jpa no historico

---
+++ historicotransacao/src/main/java/com/historico/historicotransacao/repository/HistoricoRepository.java
@@ -0,0 +1,11 @@
+package com.historico.historicotransacao.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.historico.historicotransacao.model.Historico;
+
+public interface HistoricoRepository extends JpaRepository<Historico, UUID> {
+    
+}
