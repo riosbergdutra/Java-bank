96a20309c7bb789968968225cdbbc5810bbe4953
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-28 14:57:13 -0300

    adicionando novo servico

--- historicotransacao/src/main/java/com/historico/historicotransacao/services/TransacaoService.java
+++ historicotransacao/src/main/java/com/historico/historicotransacao/services/TransacaoService.java
@@ -1,9 +1,12 @@
 package com.historico.historicotransacao.services;
 
+import java.util.UUID;
+
 import com.historico.historicotransacao.dtos.historico.req.HistoricoTransacaoRequest;
 import com.historico.historicotransacao.dtos.historico.res.HistoricoTransacaoResponse;
 
 public interface TransacaoService {
      void processarMensagemSQS(String mensagemSQS);
     HistoricoTransacaoResponse salvarTransacao(HistoricoTransacaoRequest request);
+    HistoricoTransacaoResponse buscarTransacaoPorId(UUID id);
 }
