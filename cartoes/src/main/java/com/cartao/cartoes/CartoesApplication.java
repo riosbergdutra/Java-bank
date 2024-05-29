a1aa1a85ad52664f1d37632a192ee96770af16de
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-29 12:10:20 -0300

    microservico de cartao

---
+++ cartoes/src/main/java/com/cartao/cartoes/CartoesApplication.java
@@ -0,0 +1,13 @@
+package com.cartao.cartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoesApplication.class, args);
+	}
+
+}
---
+++ cartoes/src/test/java/com/cartao/cartoes/CartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartao.cartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
