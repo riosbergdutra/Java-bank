d3834f35a284982ce7529c3f422ee1b1dfa2a8fd
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-30 15:00:11 -0300

    config para sqs

---
+++ cartoes/src/main/java/com/cartao/cartoes/config/SqsConfig.java
@@ -0,0 +1,21 @@
+package com.cartao.cartoes.config;
+
+import java.net.URI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Configuration
+public class SqsConfig {
+
+    @Bean
+   public SqsAsyncClient sqsAsyncClient() {
+    return SqsAsyncClient.builder()
+    .endpointOverride(URI.create("http://localhost:4566"))
+    .region(Region.US_EAST_1)
+    .build();
+   }
+}
