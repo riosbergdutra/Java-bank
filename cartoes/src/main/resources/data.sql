ac4d7519fd71741b672b5f218dc00edd6a07b4d6
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-30 15:01:01 -0300

    sql de cartoes

---
+++ cartoes/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS cartoes (
+    id_cartao UUID PRIMARY KEY,
+    id_usuario UUID UNIQUE REFERENCES usuarios(id_usuario),
+    id_bancaria UUID UNIQUE bancaria(id_bancaria),
+    cvv VARCHAR(255) UNIQUE,
+    status_cartao VARCHAR(50),
+    senha VARCHAR(255) UNIQUE,
+    debito VARCHAR(50),
+    credito VARCHAR(50),
+    saldo_bancaria NUMERIC NOT NULL
+);
+
