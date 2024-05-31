d67986163628039fd82da3df3357ed5187f8b6a8
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-31 16:24:18 -0300

    tirando as chaves do historico

--- historicotransacao/src/main/resources/data.sql
+++ historicotransacao/src/main/resources/data.sql
@@ -2,11 +2,9 @@ CREATE TABLE IF NOT EXISTS historico (
     id_transacao UUID PRIMARY KEY,
     id_usuarioorigem UUID NOT NULL REFERENCES usuarios(id_usuario),
     id_bancariaorigem UUID NOT NULL REFERENCES bancaria(id_bancaria),
-    chaveorigem VARCHAR(255) NOT NULL,
 
     id_bancariadestino UUID NOT NULL REFERENCES bancaria(id_bancaria),
     id_usuariodestino UUID NOT NULL  REFERENCES usuarios(id_usuario),
-    chavedestino VARCHAR(255) NOT NULL
     valor NUMERIC NOT NULL,
     tipo_transacao VARCHAR(255) NOT NULL,
     data_transacao DATE NOT NULL
