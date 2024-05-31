6bdacda9d4c591d47a1890989f35ab728f902fb1
Riosberg Dutra <riosbergduttra@gmail.com>
2024-05-31 16:21:40 -0300

    findbyidbancaria

--- cartoes/src/main/java/com/cartao/cartoes/repository/CartaoRepository.java
+++ cartoes/src/main/java/com/cartao/cartoes/repository/CartaoRepository.java
@@ -1,5 +1,6 @@
 package com.cartao.cartoes.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import com.cartao.cartoes.model.Cartao;
 @Repository
 public interface CartaoRepository extends JpaRepository<Cartao, UUID> {
     boolean existsByCvv(int cvv);
+
+    Optional<Cartao> findByIdBancaria(UUID idBancaria);
 }
 
