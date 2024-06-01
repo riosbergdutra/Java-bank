a24120ee16bb07cb7b333ead952aa8d50926e63d
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-01 14:38:58 -0300

    colocando numero do cartao

--- cartoes/src/main/resources/data.sql
+++ cartoes/src/main/resources/data.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS cartoes (
     id_usuario UUID UNIQUE REFERENCES usuarios(id_usuario),
     id_bancaria UUID UNIQUE bancaria(id_bancaria),
     cvv VARCHAR(255) UNIQUE,
+    numero_cartao VARCHAR(255) UNIQUE,
     status_cartao VARCHAR(50),
     senha VARCHAR(255) UNIQUE,
     debito VARCHAR(50),
