2ebcb5124a855823a791031f43a33904b1fc60dd
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-05 13:59:16 -0300

    colocando optional em findByEmail

--- usuarios/src/main/java/com/conta/usuarios/repository/UsuarioRepository.java
+++ usuarios/src/main/java/com/conta/usuarios/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.conta.usuarios.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,5 @@ import com.conta.usuarios.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,UUID> {
-    Usuario findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 } 
