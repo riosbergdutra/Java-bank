e48cb0f3d98c3394fe93f25db8ca6f7328707d1d
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-05 14:06:01 -0300

    preautorize para transacao

--- historicotransacao/src/main/java/com/historico/historicotransacao/controllers/TransacaoController.java
+++ historicotransacao/src/main/java/com/historico/historicotransacao/controllers/TransacaoController.java
@@ -2,9 +2,12 @@ package com.historico.historicotransacao.controllers;
 
 import com.historico.historicotransacao.dtos.historico.res.HistoricoTransacaoResponse;
 import com.historico.historicotransacao.services.TransacaoService;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,7 @@ public class TransacaoController {
         this.transacaoService = transacaoService;
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_BASIC')")
     @GetMapping("/transacoes/{id}")
     public ResponseEntity<HistoricoTransacaoResponse> buscarTransacaoPorId(@PathVariable UUID id) {
         try {
