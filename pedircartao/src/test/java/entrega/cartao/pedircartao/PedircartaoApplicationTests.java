b3fb4ad54c15da756a2df369ae9ca011324d0a5d
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-09 11:05:47 -0300

    pasta de teste

---
+++ pedircartao/src/test/java/entrega/cartao/pedircartao/PedircartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package entrega.cartao.pedircartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedircartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
