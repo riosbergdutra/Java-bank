3b7858933093da56bfd9fefe74a6c37c9d595228
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-09 11:08:21 -0300

    arquivo main

---
+++ pedircartao/src/main/java/entrega/cartao/pedircartao/PedircartaoApplication.java
@@ -0,0 +1,13 @@
+package entrega.cartao.pedircartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedircartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedircartaoApplication.class, args);
+	}
+
+}
