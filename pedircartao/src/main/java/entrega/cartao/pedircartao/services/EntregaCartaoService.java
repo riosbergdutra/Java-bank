ff3395e80af742f01706dad97829d9bbfea1e6ee
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-09 11:10:00 -0300

    service de entregacartao

---
+++ pedircartao/src/main/java/entrega/cartao/pedircartao/services/EntregaCartaoService.java
@@ -0,0 +1,9 @@
+package entrega.cartao.pedircartao.services;
+
+public class EntregaCartaoService {
+
+    public void processarMensagemSQSCriarEntregaCartao(String mensagemSQS) {
+        
+    }
+    
+}
