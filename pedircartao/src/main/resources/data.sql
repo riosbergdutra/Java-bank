81d4e694b94b2f9e81af4acbde812254d1d7eaa5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-09 11:06:35 -0300

    tabela de entregacartao

---
+++ pedircartao/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS entregacartao (
+    id_entrega UUID PRIMARY KEY,
+    id_cartao UUID UNIQUE,
+    id_usuario UUID UNIQUE,
+    rua VARCHAR(255) NOT NULL,
+    cidade VARCHAR(255) NOT NULL,
+    estado VARCHAR(255) NOT NULL,
+    cep VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL,
+    complemento VARCHAR(255) NOT NULL
+);
