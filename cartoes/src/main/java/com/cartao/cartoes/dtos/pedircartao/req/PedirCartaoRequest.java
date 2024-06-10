50edd127f5a70b9856ac36ab264587aa4476b1a0
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-10 16:17:06 -0300

    adicionando validation no request

--- cartoes/src/main/java/com/cartao/cartoes/dtos/pedircartao/req/PedirCartaoRequest.java
+++ cartoes/src/main/java/com/cartao/cartoes/dtos/pedircartao/req/PedirCartaoRequest.java
@@ -1,17 +1,20 @@
 package com.cartao.cartoes.dtos.pedircartao.req;
 
 
-import java.util.UUID;
 
 import com.cartao.cartoes.enums.EstadoBrasil;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record PedirCartaoRequest(
-        UUID idCartao,
-        String rua,
-        String cidade,
-        EstadoBrasil estado,
-        String cep,
-        String numero,
-        String complemento) {
+    @NotEmpty @Size(min = 1, max = 100) String rua,
+    @NotEmpty @Size(min = 1, max = 50) String cidade,
+    @NotNull EstadoBrasil estado,
+    @NotEmpty @Pattern(regexp = "\\d{5}-\\d{3}") String cep,
+    @NotEmpty @Size(min = 1, max = 10) String numero,
+    String complemento) {
 
 }
