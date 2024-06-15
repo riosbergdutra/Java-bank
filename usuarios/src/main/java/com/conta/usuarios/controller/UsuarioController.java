b99843e639d2fc5b1491af3bc96abc1f0881a86a
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-15 15:10:16 -0300

    colocando autorizacao para o service

--- usuarios/src/main/java/com/conta/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/com/conta/usuarios/controller/UsuarioController.java
@@ -23,78 +23,56 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-
     @PostMapping("/criar")
     public ResponseEntity<UsuarioResponseDto> criarUsuario(@RequestBody UsuarioRequestDto usuarioDto) {
         UsuarioResponseDto responseDto = usuarioService.criarUsuarioService(usuarioDto);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
-   // Buscar usuário por ID
-   @PreAuthorize("hasAuthority('SCOPE_USER')")
-   @GetMapping("/{id}")
-   public ResponseEntity<UsuarioResponseDto> buscarUsuarioPorId(@PathVariable UUID id, Authentication authentication) {
-       UUID userId = UUID.fromString(authentication.getName());
-       // Verificar se o escopo do token JWT é adequado para acessar este recurso e se o ID do usuário é correspondente
-       if (!authentication.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("SCOPE_USER")) || !userId.equals(id)) {
-           return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-       }
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> buscarUsuarioPorId(@PathVariable UUID id, Authentication authentication) {
+        UUID userId = UUID.fromString(authentication.getName());
+        UsuarioResponseDto responseDto = usuarioService.buscarUsuarioPorIdService(id, userId);
+        if (responseDto != null) {
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
-       // Prossiga com a busca do usuário por ID
-       UsuarioResponseDto responseDto = usuarioService.buscarUsuarioPorIdService(id);
-       if (responseDto != null) {
-           return new ResponseEntity<>(responseDto, HttpStatus.OK);
-       } else {
-           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-       }
-   }
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    @GetMapping("/listar")
+    public ResponseEntity<List<UsuarioResponseDto>> listarUsuarios(Authentication authentication) {
+        if (!authentication.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("SCOPE_ADMIN"))) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        List<UsuarioResponseDto> usuarios = usuarioService.listarUsuariosService();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
 
-   // Listar usuários
-   @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
-   @GetMapping("/listar")
-   public ResponseEntity<List<UsuarioResponseDto>> listarUsuarios(Authentication authentication) {
-       // Verificar se o escopo do token JWT é adequado para acessar este recurso
-       if (!authentication.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("SCOPE_ADMIN"))) {
-           return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-       }
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PutMapping("/senha/{id}")
+    public ResponseEntity<UsuarioSenhaResDto> atualizarUsuarioSenha(@PathVariable UUID id,
+            @RequestBody UsuarioSenhaReqDto usuarioDto, Authentication authentication) {
+        UUID userId = UUID.fromString(authentication.getName());
+        UsuarioSenhaResDto responseDto = usuarioService.atualizarUsuarioSenhaService(id, userId, usuarioDto);
+        if (responseDto != null) {
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
-       // Prossiga com a listagem de usuários
-       List<UsuarioResponseDto> usuarios = usuarioService.listarUsuariosService();
-       return new ResponseEntity<>(usuarios, HttpStatus.OK);
-   }
-
-   // Atualizar senha do usuário
-   @PreAuthorize("hasAuthority('SCOPE_USER')")
-   @PutMapping("/senha/{id}")
-   public ResponseEntity<UsuarioSenhaResDto> atualizarUsuarioSenha(@PathVariable UUID id, @RequestBody UsuarioSenhaReqDto usuarioDto, Authentication authentication) {
-       UUID userId = UUID.fromString(authentication.getName());
-       // Verificar se o escopo do token JWT é adequado para acessar este recurso e se o ID do usuário é correspondente
-       if (!authentication.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("SCOPE_USER")) || !userId.equals(id)) {
-           return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-       }
-
-       // Prossiga com a atualização da senha do usuário
-       UsuarioSenhaResDto responseDto = usuarioService.atualizarUsuarioSenhaService(id, usuarioDto);
-       if (responseDto != null) {
-           return new ResponseEntity<>(responseDto, HttpStatus.OK);
-       } else {
-           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-       }
-   }
-
-   // Deletar usuário
-   @PreAuthorize("hasAuthority('SCOPE_USER')")
-   @DeleteMapping("/deletar/{id}")
-   public ResponseEntity<Void> deletarUsuario(@PathVariable UUID id, Authentication authentication) {
-       UUID userId = UUID.fromString(authentication.getName());
-       // Verificar se o escopo do token JWT é adequado para acessar este recurso e se o ID do usuário é correspondente
-       if (!authentication.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("SCOPE_USER")) || !userId.equals(id)) {
-           return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-       }
-
-       // Prossiga com a exclusão do usuário
-       usuarioService.deletarUsuarioService(id);
-       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-   }
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable UUID id, Authentication authentication) {
+        UUID userId = UUID.fromString(authentication.getName());
+        try {
+            usuarioService.deletarUsuarioService(id, userId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
-
--- usuarios/src/main/java/com/conta/usuarios/services/UsuarioService.java
+++ usuarios/src/main/java/com/conta/usuarios/services/UsuarioService.java
@@ -35,9 +35,7 @@ public class UsuarioService {
     public UsuarioResponseDto criarUsuarioService(UsuarioRequestDto usuarioDto) {
         Set<Usuario.Role> roles = Set.of(Usuario.Role.USER);
         // Obtém a data atual
-        LocalDate dataAtual = LocalDate.now();
         // se não for fornecida pelo dto gera uma data atual
-        LocalDate dataConta = usuarioDto.dataConta() != null ? usuarioDto.dataConta() : dataAtual;
         
         // Cria um novo usuário com base nos dados fornecidos no DTO de requisição
         Usuario novoUsuario = new Usuario();
@@ -48,7 +46,7 @@ public class UsuarioService {
         novoUsuario.setCpf(usuarioDto.cpf());
         novoUsuario.setRoles(roles);
         novoUsuario.setTipoConta(usuarioDto.tipoConta());
-        novoUsuario.setDataConta(dataConta);
+        novoUsuario.setDataConta(LocalDate.now());
 
         // Salva o novo usuário no banco de dados
         Usuario usuarioCriado = usuarioRepository.save(novoUsuario);
@@ -72,11 +70,12 @@ public class UsuarioService {
         );
     }
 
-    public UsuarioResponseDto buscarUsuarioPorIdService(UUID id) {
-        // Busca um usuário pelo ID no banco de dados
+    public UsuarioResponseDto buscarUsuarioPorIdService(UUID id, UUID userId) {
+        if (!id.equals(userId)) {
+            return null;
+        }
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
 
-        // Verifica se o usuário foi encontrado
         if (optionalUsuario.isPresent()) {
             Usuario usuario = optionalUsuario.get();
             return new UsuarioResponseDto(
@@ -91,10 +90,7 @@ public class UsuarioService {
     }
 
     public List<UsuarioResponseDto> listarUsuariosService() {
-        // Lista todos os usuários do banco de dados
         List<Usuario> usuarios = usuarioRepository.findAll();
-
-        // Converte a lista de usuários para uma lista de DTOs de resposta
         return usuarios.stream()
                 .map(usuario -> new UsuarioResponseDto(
                         usuario.getNome(),
@@ -106,56 +102,43 @@ public class UsuarioService {
                 .collect(Collectors.toList());
     }
 
-    public UsuarioSenhaResDto atualizarUsuarioSenhaService(UUID id, UsuarioSenhaReqDto usuarioDto) {
-        // Busca o usuário pelo ID no banco de dados
+    public UsuarioSenhaResDto atualizarUsuarioSenhaService(UUID id, UUID userId, UsuarioSenhaReqDto usuarioDto) {
+        if (!id.equals(userId)) {
+            return null;
+        }
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
 
-        // Verifica se o usuário foi encontrado
         if (optionalUsuario.isPresent()) {
             Usuario usuario = optionalUsuario.get();
-
-            // Atualiza os dados do usuário com base nos dados fornecidos no DTO de requisição
             String senhaHash = passwordEncoder.encode(usuarioDto.senha());
             usuario.setSenha(senhaHash);
-
-            // Salva as alterações no banco de dados
             usuarioRepository.save(usuario);
-
-            // Retorna um DTO de resposta com os dados atualizados do usuário
             return new UsuarioSenhaResDto("senha mudada com sucesso");
         }
         return null;
     }
 
-    public void deletarUsuarioService(UUID id) {
-        try {
-            // Busca o usuário pelo ID no banco de dados
-            Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+    public void deletarUsuarioService(UUID id, UUID userId) {
+        if (!id.equals(userId)) {
+            throw new IllegalArgumentException("Usuário não autorizado");
+        }
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
 
-            // Verifica se o usuário foi encontrado
-            if (optionalUsuario.isPresent()) {
-                Usuario usuario = optionalUsuario.get();
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            String queueUrl = "http://localhost:4566/000000000000/usuarios";
+            String messageBody = "idUsuario: " + usuario.getId_usuario() + "\n" +
+                    "acao: deletar";
 
-                // Envia a mensagem para a fila SQS com os dados do usuário
-                String queueUrl = "http://localhost:4566/000000000000/usuarios";
-                String messageBody = "idUsuario: " + usuario.getId_usuario() + "\n" +
-                        "acao: deletar";
-
-                sqsTemplate.send(queueUrl, messageBody);
-
-                // Remove o usuário do banco de dados pelo ID
-                usuarioRepository.deleteById(id);
-            } else {
-                System.out.println("Usuário não encontrado para o ID fornecido.");
-            }
-        } catch (Exception e) {
-            // Tratar exceções
-            e.printStackTrace();
+            sqsTemplate.send(queueUrl, messageBody);
+            usuarioRepository.deleteById(id);
         }
     }
+
     public Optional<Usuario> loginService(String email) {
-        return (usuarioRepository.findByEmail(email));
+        return usuarioRepository.findByEmail(email);
     }
+
     public BCryptPasswordEncoder getPasswordEncoder() {
         return passwordEncoder;
     }
