43d52242345bf21a79361903a9d50693065de245
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-17 13:29:25 -0300

    tirando a data da transacao do request

--- contabancaria/src/main/java/com/bancaria/contabancaria/dtos/transferencia/request/TransacaoRequestDto.java
+++ contabancaria/src/main/java/com/bancaria/contabancaria/dtos/transferencia/request/TransacaoRequestDto.java
@@ -1,7 +1,6 @@
 package com.bancaria.contabancaria.dtos.transferencia.request;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 import com.bancaria.contabancaria.enums.TipoTransacao;
 //requisição da transacão
@@ -9,6 +8,5 @@ public record TransacaoRequestDto(
     TipoTransacao tipoTransacao,
     String ChaveOrigem,
     String ChaveDestino,
-    BigDecimal valor,
-    LocalDate dataTransacao
+    BigDecimal valor
 ) {}
