edff739d1b2cdbd4d65f15985cb533677f24431a
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-17 13:28:30 -0300

    achar todas as transacoes pelo id do usuario origem ou destino

--- historicotransacao/src/main/java/com/historico/historicotransacao/repository/HistoricoRepository.java
+++ historicotransacao/src/main/java/com/historico/historicotransacao/repository/HistoricoRepository.java
@@ -1,5 +1,6 @@
 package com.historico.historicotransacao.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.historico.historicotransacao.model.Historico;
 
 public interface HistoricoRepository extends JpaRepository<Historico, UUID> {
+
+    List<Historico> findAllByIdUsuarioOrigemOrIdUsuarioDestino(UUID idUsuario, UUID idUsuario2);
 }
