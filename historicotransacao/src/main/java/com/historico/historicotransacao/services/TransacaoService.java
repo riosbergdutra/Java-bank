742bdd6947b8ca5520a83262bf6bcfa335cb9cb5
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-17 13:26:27 -0300

    lista de todo o historico do usuarioservice

--- historicotransacao/src/main/java/com/historico/historicotransacao/services/TransacaoService.java
+++ historicotransacao/src/main/java/com/historico/historicotransacao/services/TransacaoService.java
@@ -1,5 +1,6 @@
 package com.historico.historicotransacao.services;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.historico.historicotransacao.dtos.historico.req.HistoricoTransacaoRequest;
@@ -9,4 +10,5 @@ public interface TransacaoService {
      void processarMensagemSQS(String mensagemSQS);
     HistoricoTransacaoResponse salvarTransacao(HistoricoTransacaoRequest request);
     HistoricoTransacaoResponse buscarTransacaoPorId(UUID id);
+    List<HistoricoTransacaoResponse> transacoesUsuario(UUID idUsuario);  
 }
