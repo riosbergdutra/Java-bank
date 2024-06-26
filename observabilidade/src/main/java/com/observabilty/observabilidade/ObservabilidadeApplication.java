e8a9b089fac95b49148e6f2b2b767619877391b3
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-26 14:16:50 -0300

    arquivo de inicializacao do spring

---
+++ observabilidade/src/main/java/com/observabilty/observabilidade/ObservabilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.observabilty.observabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilidadeApplication.class, args);
+	}
+
+}
