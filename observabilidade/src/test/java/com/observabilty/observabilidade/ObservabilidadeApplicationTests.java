26ade4f7a58a3b7e3071ef56b72ca9d1b562dcfc
Riosberg Dutra <riosbergduttra@gmail.com>
2024-06-30 10:41:49 -0300

    configurando observabilidade

---
+++ observabilidade/src/test/java/com/observabilty/observabilidade/ObservabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.observabilty.observabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
