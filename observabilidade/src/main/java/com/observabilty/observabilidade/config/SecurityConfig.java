1a4531d281b00d1c0f51fc3bb20f9dd34bd9c290
Riosberg Dutra <riosbergduttra@gmail.com>
2024-07-02 13:58:49 -0300

    chave publica para observabilidade

--- observabilidade/src/main/java/com/observabilty/observabilidade/config/SecurityConfig.java
+++ observabilidade/src/main/java/com/observabilty/observabilidade/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.observabilty.observabilidade.config;
 
-import java.security.interfaces.RSAPublicKey;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
@@ -38,4 +38,5 @@ public class SecurityConfig {
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
+
 }
